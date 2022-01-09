a3799f8b8479e7579fe61b6dac2ac3295e1afcca
Shane Jonas <jonas.shane@gmail.com>
2022-01-09 12:46:26 -0800

    Changed confirm snap to be configurable via params

--- packages/confirm/src/index.ts
+++ packages/confirm/src/index.ts
@@ -1,10 +1,16 @@
 // eslint-disable-next-line import/unambiguous
 wallet.registerRpcMessageHandler(async (_originString, requestObject) => {
   switch (requestObject.method) {
-    case 'hello':
+    case 'confirm':
       return wallet.request({
         method: 'snap_confirm',
-        params: [{ prompt: 'Hello' }],
+        params: [
+          {
+            prompt: requestObject.params[0] || 'Hello',
+            description: requestObject.params[1],
+            textAreaContent: requestObject.params[2],
+          },
+        ],
       });
     default:
       throw new Error('Method not found.');
