ec64252567d1abe41af5964fb87dd2c257943903
Bowen Sanders <bowensanders@gmail.com>
2022-05-10 11:49:30 -0700

    create manageState test-snap and interface (#33)
    * create manageState snap and interface
    
    * fixed unfinished retrieve and clear cases
    
    * Update packages/manageState/src/index.ts
    
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>
    
    * Update packages/manageState/CHANGELOG.md
    
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>
    
    * Update packages/manageState/CHANGELOG.md
    
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>
    
    * Update packages/manageState/CHANGELOG.md
    
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>
    
    * latest build changes to shasum
    
    * cleaned up unnecessaries
    
    * fixed accidental removal of unambiguous
    
    * fixed manifest shasum
    
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>

---
+++ packages/manageState/src/index.ts
@@ -0,0 +1,42 @@
+/// <reference path="../../../types/wallet.d.ts" />
+
+// eslint-disable-next-line import/unambiguous
+wallet.registerRpcMessageHandler(async (_originString, requestObject) => {
+  let state = (await wallet.request({
+    method: 'snap_manageState',
+    params: ['get'],
+  })) as { testState: string[] } | null;
+
+  if (!state) {
+    state = { testState: [] };
+    // initialize state if empty and set default data
+    await wallet.request({
+      method: 'snap_manageState',
+      params: ['update', state],
+    });
+  }
+
+  switch (requestObject.method) {
+    case 'storeTestData':
+      state.testState.push(requestObject.params[0]);
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['update', state],
+      });
+      return true;
+    case 'retrieveTestData':
+      return await wallet.request({
+        method: 'snap_manageState',
+        params: ['get'],
+      });
+    case 'clearTestData':
+      await wallet.request({
+        method: 'snap_manageState',
+        params: ['clear'],
+      });
+      return true;
+
+    default:
+      throw new Error('Method not found.');
+  }
+});
