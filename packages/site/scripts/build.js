02cb1110f948709f65eb190f16ea0bf621c8bb58
bowensanders <bowensanders@gmail.com>
2022-05-23 12:14:26 -0700

    removed build copying of removed file

--- packages/site/scripts/build.js
+++ packages/site/scripts/build.js
@@ -6,7 +6,6 @@ const FileNames = {
   icon: 'icon.svg',
   style: 'style.css',
   index: 'index.html',
-  script: 'test-snaps.js',
   image: 'snaps-ascii-to-logo.gif',
   font1: 'EuclidCircularB-Bold.woff',
   font2: 'EuclidCircularB-Regular.woff',
@@ -38,7 +37,6 @@ async function main(mode) {
   await mkdirp(distPath);
   await copyToDist(FileNames.icon);
   await copyToDist(FileNames.style);
-  await copyToDist(FileNames.script);
   await copyToDist(FileNames.image);
   await copyToDist(FileNames.font1);
   await copyToDist(FileNames.font2);
