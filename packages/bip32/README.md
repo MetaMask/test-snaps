3f3fc91bfa66d21ad440a109b0fd1bcd24b250a6
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-08-16 15:58:25 +0200

    Add BIP-32 test snap

---
+++ packages/bip32/README.md
@@ -0,0 +1,3 @@
+# BIP-32 example snap
+
+This Snap demonstrates how to derive BIP-32 private keys.
---
+++ packages/bip32/src/index.ts
@@ -0,0 +1,32 @@
+import { ethErrors } from 'eth-rpc-errors';
+import { JsonSLIP10Node, SLIP10Node } from '@metamask/key-tree';
+import { OnRpcRequestHandler } from '@metamask/snap-types';
+
+interface GetAccountParams {
+  path: string;
+  curve?: 'secp256k1' | 'ed25519';
+
+  [key: string]: unknown;
+}
+
+const getPrivateKey = async (params: GetAccountParams): Promise<string> => {
+  const json = (await wallet.request({
+    method: 'snap_getBip32Entropy',
+    params,
+  })) as JsonSLIP10Node;
+
+  const node = await SLIP10Node.fromJSON(json);
+  return node.privateKey as string;
+};
+
+export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
+  switch (request.method) {
+    case 'getAccount':
+      return await getPrivateKey(request.params as GetAccountParams);
+
+    default:
+      throw ethErrors.rpc.methodNotFound({
+        data: { method: request.method },
+      });
+  }
+};
