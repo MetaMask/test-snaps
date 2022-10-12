88c240c79fec0e83e4226bc763f203f4bfadaa1f
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-10-12 14:40:41 +0200

    Fix installed snaps

--- packages/site/src/features/installed-snaps/Snaps.tsx
+++ packages/site/src/features/installed-snaps/Snaps.tsx
@@ -3,15 +3,13 @@ import { useGetSnapsQuery } from '../../api';
 import { Result, Snap } from '../../components';
 
 export const Snaps: FunctionComponent = () => {
-  const { data: installedSnaps } = useGetSnapsQuery(undefined, {
-    pollingInterval: 1000,
-  });
+  const { data: installedSnaps } = useGetSnapsQuery();
 
   return (
     <Snap name="Installed Snaps" testId="installed-snaps" hideConnect={true}>
       <Result>
         <span data-testid="installed-snaps-result">
-          {installedSnaps
+          {installedSnaps && Object.keys(installedSnaps).length > 0
             ? Object.keys(installedSnaps).join(', ')
             : 'No Snaps installed.'}
         </span>
