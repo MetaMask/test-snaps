c49fdc42ad2cff107036f1fc2d76335b46ae5919
Hassan Malik <41640681+hmalik88@users.noreply.github.com>
2023-02-15 16:02:53 -0500

    Fix dialog snap & site code (#138)
    * update dialog snap
    
    * fix versions that need to be fetched
    
    * update manifest

--- packages/site/src/features/update-snap/Update.tsx
+++ packages/site/src/features/update-snap/Update.tsx
@@ -8,8 +8,8 @@ import {
 } from '../../api';
 
 const UPDATE_SNAP_ID = 'npm:@metamask/test-snap-bip32';
-const UPDATE_SNAP_OLD_VERSION = '4.0.1';
-const UPDATE_SNAP_NEW_VERSION = '4.0.2';
+const UPDATE_SNAP_OLD_VERSION = '4.6.3';
+const UPDATE_SNAP_NEW_VERSION = '4.6.4';
 
 export const Update: FunctionComponent = () => {
   const [installSnap, { isLoading }] = useInstallSnapMutation();
