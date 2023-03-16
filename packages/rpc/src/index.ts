e4d03583f1f544f4fde02781b8d3e4ba7cb503e8
Frederik Bolding <frederik.bolding@gmail.com>
2023-03-16 13:13:46 +0100

    Stop using `wallet_requestSnaps` inside snaps (#152)
    * Stop using wallet_requestSnaps inside snaps
    
    * Fix lint

--- packages/rpc/src/index.ts
+++ packages/rpc/src/index.ts
@@ -1,38 +1,12 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import packageJson from '../package.json';
-
-const OTHER_SNAP_ID = 'npm:@metamask/test-snap-bip32';
-
-/**
- * Request access to {@link OTHER_SNAP_ID} if it is not already connected.
- */
-const requestSnap = async () => {
-  const snaps = (await snap.request({ method: 'wallet_getSnaps' })) as Record<
-    string,
-    unknown
-  >;
-
-  if (!snaps[OTHER_SNAP_ID]) {
-    await snap.request({
-      method: 'wallet_requestSnaps',
-      params: {
-        [OTHER_SNAP_ID]: {
-          version: packageJson.version,
-        },
-      },
-    });
-  }
-};
 
 export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
   switch (request.method) {
     case 'send': {
-      await requestSnap();
-
       return snap.request({
         method: 'wallet_invokeSnap',
         params: {
-          snapId: OTHER_SNAP_ID,
+          snapId: 'npm:@metamask/test-snap-bip32',
           request: {
             method: 'getPublicKey',
             params: {
