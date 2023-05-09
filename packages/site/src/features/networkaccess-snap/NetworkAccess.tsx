cf91e3af8b146ccca7c83f63f0e9345ced645e01
bowensanders <bowensanders@gmail.com>
2023-05-09 16:51:22 -0700

    fixed lint

--- packages/site/src/features/networkaccess-snap/NetworkAccess.tsx
+++ packages/site/src/features/networkaccess-snap/NetworkAccess.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from 'react';
 import { Button } from 'react-bootstrap';
-import { Result, Snap } from '../../components';
+import { Snap } from '../../components';
 import { useInvokeMutation } from '../../api';
 import { getSnapId } from '../../utils';
 
@@ -8,7 +8,7 @@ const RPC_SNAP_ID = 'npm:@metamask/test-snap-networkaccess';
 const RPC_SNAP_PORT = 8012;
 
 export const NetworkAccess: FunctionComponent = () => {
-  const [invokeSnap, { isLoading, data, error }] = useInvokeMutation();
+  const [invokeSnap, { isLoading }] = useInvokeMutation();
 
   const handleSubmit = () => {
     invokeSnap({
