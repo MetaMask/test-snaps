fb3608cc2542daeca2cd7ab2f51a058035f771b2
bowensanders <bowensanders@gmail.com>
2023-05-09 17:16:28 -0700

    more fixes

--- packages/networkaccess/src/index.ts
+++ packages/networkaccess/src/index.ts
@@ -3,14 +3,16 @@ import { panel, text } from '@metamask/snaps-ui';
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 async function getPage() {
-  const response = await fetch('https://metamask.github.io/test-snaps/0.1.3/'); 
+  const response = await fetch(
+    'https://metamask.github.io/test-snaps/index.html',
+  );
   return response.text();
 }
 
 export const onRpcRequest: OnRpcRequestHandler = ({ request }) => {
   switch (request.method) {
     case 'networkAccessTest':
-      return getPage().then(page => {
+      return getPage().then((page) => {
         return snap.request({
           method: 'snap_dialog',
           params: {
