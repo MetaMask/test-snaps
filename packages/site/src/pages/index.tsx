48920e4283a0edd65cd6d4294a88d037aaf25ee4
Bowen Sanders <bowensanders@gmail.com>
2023-05-19 09:50:37 -0700

    Create networkAccess snap (#168)

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -15,6 +15,7 @@ import { Rpc } from '../features/rpc-snap';
 import { Insights } from '../features/insights-snap';
 import { WebAssembly } from '../features/wasm-snap';
 import { GetEntropy } from '../features/getentropy-snap';
+import { NetworkAccess } from '../features/networkaccess-snap';
 import { Ethprovider } from '../features/ethprovider-snap';
 
 interface Query {
@@ -47,6 +48,7 @@ const Index: FunctionComponent = () => {
         <Cronjob />
         <WebAssembly />
         <GetEntropy />
+        <NetworkAccess />
         <Ethprovider />
       </Row>
     </Container>
