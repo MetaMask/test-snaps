9484cecfadf4e0db55dc7094ccc37867813f0c07
BeroBurny <bero4net@gmail.com>
2023-06-05 11:37:43 +0200

    fix error snap

--- packages/error/src/index.ts
+++ packages/error/src/index.ts
@@ -2,7 +2,7 @@ import { OnRpcRequestHandler } from '@metamask/snaps-types';
 
 export const onRpcRequest: OnRpcRequestHandler = async () => {
   // eslint-disable-next-line no-new
-  new Promise((resolve) => {
+  await new Promise((resolve) => {
     let num = 0;
     while (num < 100) {
       // eslint-disable-next-line no-plusplus
